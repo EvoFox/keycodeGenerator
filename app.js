d80cea9e42d5b78b0db4584bfb40c416138d707b
EvoFox <foulkes.joshua@live.co.uk>
2022-05-23 14:16:01 +0100

    Added History Table

--- app.js
+++ app.js
@@ -9,8 +9,20 @@ window.addEventListener("keydown", (e) => {
     const key = document.getElementById("key");
     const which = document.getElementById("which");
     const log = document.getElementById("heading");
+    const history = document.getElementById("history-table");
+
+    let row = history.insertRow(0);
+
+    let histCode = row.insertCell(0);
+    let histKey = row.insertCell(1);
+    let histWhich = row.insertCell(2);
+
+    histCode.textContent = `${e.code}`;
+    histKey.textContent = `${e.key}`;
+    histWhich.textContent = `${e.which}`;
 
     log.textContent = `${e.key.toUpperCase()} was pressed!`;
+
 
     code.textContent = e.code;
     key.textContent = e.key;
