ac469f2b2e8b9e8c12fea805b7376e385a6f7f75
EvoFox <foulkes.joshua@live.co.uk>
2022-05-23 12:31:43 +0100

    Initial Formatting

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 window.addEventListener("keydown", (e) => {
+    const firstPage = document.getElementById("first-page");
+    const secondPage = document.getElementById("second-page");
+
+    firstPage.style.display = "none"
+    secondPage.style.display = "flex"
+
     const code = document.getElementById("code");
     const key = document.getElementById("key");
     const which = document.getElementById("which");
+    const log = document.getElementById("heading");
+
+    log.textContent = `${e.key.toUpperCase()} was pressed!`;
 
     code.textContent = e.code;
     key.textContent = e.key;
